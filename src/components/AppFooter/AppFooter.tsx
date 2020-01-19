81a812c79f224646ded44ba93501401d71feca00
Erdoğan BULUT <e-bulut@hotmail.com.tr>
2020-01-19 16:26:44 +0300

    Fix footer ui schema

--- src/components/AppFooter/AppFooter.tsx
+++ src/components/AppFooter/AppFooter.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
-const AppFooter: React.FC = () => <p>AppFooter...</p>;
+import { Icon } from 'antd';
+
+import './AppFooter.scss';
+
+const AppFooter: React.FC = () => (
+  <p className="component is-footer">
+    &nbsp;
+    <a
+      href="https://github.com/erdoganbulut/gtr-todo-app"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Icon type="github" />
+    </a>
+  </p>
+);
 
 export default AppFooter;
