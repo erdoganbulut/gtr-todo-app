7ad87a21831d46e4fc03f868948f840839e3759b
Erdoğan BULUT <e-bulut@hotmail.com.tr>
2020-01-19 15:37:01 +0300

    Fix header ui schema

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
-const AppHeader: React.FC = () => <p>AppHeader...</p>;
+import { Menu } from 'antd';
+
+import './AppHeader.scss';
+
+const AppHeader: React.FC = () => (
+  <header>
+    <div className="header--logo">GTR To-do App</div>
+    <Menu className="header--menu" theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+      <Menu.Item key="1">Home</Menu.Item>
+      <Menu.Item key="2">Detail</Menu.Item>
+      <Menu.Item key="3">Documentation</Menu.Item>
+    </Menu>
+  </header>
+);
 
 export default AppHeader;
