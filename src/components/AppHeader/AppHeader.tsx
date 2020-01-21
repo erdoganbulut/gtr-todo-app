cffab5ca6bf1136312a3915d9060773171db7fd4
Erdoğan BULUT <e-bulut@hotmail.com.tr>
2020-01-21 20:48:51 +0300

    Fix navigation for dynamic

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 
+import { useLocation, Link } from 'react-router-dom';
+
 import { Menu } from 'antd';
 
 import './AppHeader.scss';
 
-const AppHeader: React.FC = () => (
-  <header>
-    <div className="header--logo">GTR To-do App</div>
-    <Menu className="header--menu" theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-      <Menu.Item key="1">Home</Menu.Item>
-      <Menu.Item key="2">Detail</Menu.Item>
-      <Menu.Item key="3">Documentation</Menu.Item>
-    </Menu>
-  </header>
-);
+const AppHeader: React.FC = () => {
+  const location = useLocation();
+  return (
+    <header>
+      <div className="header--logo">GTR To-do App</div>
+      <Menu
+        className="header--menu"
+        theme="dark"
+        mode="horizontal"
+        selectedKeys={[location.pathname]}
+      >
+        <Menu.Item key="/">
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item key="/documentation">
+          <Link to="/documentation">Documentation</Link>
+        </Menu.Item>
+      </Menu>
+    </header>
+  );
+};
 
 export default AppHeader;
