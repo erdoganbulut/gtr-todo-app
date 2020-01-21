d77b1ac0277ca4023c6051dec0300b03ac8ea073
Erdoğan BULUT <e-bulut@hotmail.com.tr>
2020-01-21 16:38:41 +0300

    Add redux dev tools support

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,10 +1,11 @@
 import { applyMiddleware, createStore, Store } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { ApplicationStateI, rootReducer, rootSaga } from './modules/index';
 import sagaMiddleware from './middlewares/sagas.middleware';
 
 export default function configureStore(initialState: ApplicationStateI): Store<ApplicationStateI> {
   const middlewares = applyMiddleware(sagaMiddleware);
-  const store = createStore(rootReducer, initialState, middlewares);
+  const store = createStore(rootReducer, initialState, composeWithDevTools(middlewares));
 
   sagaMiddleware.run(rootSaga);
 
